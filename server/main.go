fd4e257ab4516ecf705f025a417bc5aab6428bba
HowardKiat <buisnesskiat123@gmail.com>
2024-02-19 15:42:40 +0800

    Yea no idea what im doing rn

--- server/main.go
+++ server/main.go
@@ -4,7 +4,9 @@ import (
 	"bufio"
 	"fmt"
 	"log"
+	"net"
 	"os"
+	"strings"
 )
 
 func main() {
@@ -24,5 +26,34 @@ func main() {
 }
 
 func checkDomain(domain string) {
+	// bool = boolean
+
+	var hasMX, hasSPF, hasDMARC bool
+	var spfRecord, dmarcRecord string
+
+	mxRecords, err := net.LookupMX(domain)
+
+	if err != nil {
+		log.Printf("Error: %v\n", err)
+	}
+
+	if len(mxRecords) > 0 {
+		hasMX = true
+	}
+
+	txtRecords, err := net.LookupTXT(domain)
+
+	if err != nil {
+		log.Printf("Error: %v\n", err)
+	}
+
+	for _, record := range txtRecords {
+		if strings.HasPrefix(record, "v=spfi") {
+			hasSPF = true
+			spfRecord = record
+			break
+		}
+
+	}
 
 }
